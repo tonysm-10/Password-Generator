28ab959620c9c8c8bf17f786026e576720a1795b
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-03-13 21:28:30 -0700

    added and linked javascript file, did all the javascript code so that my password generator works

--- script.js
+++ script.js
@@ -1,17 +1,88 @@
 // Assignment code here
+var generateBtn = document.getElementById("generate");
+var uppercaseCharCode = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowercaseCharCode = 'abcdefghijklmnopqrstuvwxyz';
+var numberCharCode = '1234567890';
+var symbolCharCode = '@,.+-=/*()';
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+
+
+
+
+//write code to generate the password
+function generatePassword() {
+
+  function getCharacterAmount() {
+    let charactersAmount = prompt("how many character would you like in you password?(Input 8-126)");
+    if (!(charactersAmount >= 8 && charactersAmount <= 126)) {
+      alert("The number you input has to be between 8 and 126")
+     getCharacterAmount()
+    }
+  
+    if (isNaN(charactersAmount)){
+      getCharacterAmount();
+     
+    }
+   return charactersAmount;
+   
+  }
+
+  let charactersAmount = getCharacterAmount();
+
+
+  // lowercaseCharCode = prompt("do you want lowercase letters in your password?")
+
+  let includeNumbers = confirm("Do Yyou want to include number?")
+
+  let includeUppercase = confirm("Do you want uppercase letters in your password?")
+
+  let includeSymbols = confirm("Do you want symbols in your password?")
+
+
+  console.log(numberCharCode)
+  var charcodes = lowercaseCharCode
+  if (includeNumbers) charcodes = charcodes.concat(numberCharCode)
+  if (includeSymbols) charcodes = charcodes.concat(symbolCharCode)
+  if (includeUppercase) charcodes = charcodes.concat(uppercaseCharCode)
+  var passwordCharacter = []
+  for (let i = 0; i < charactersAmount; i++) {
+    var randomIndex = Math.floor(Math.random() * charcodes.length)
+    var randomCharacter = charcodes.charAt(randomIndex);
+    passwordCharacter.push(randomCharacter)
+
+  }
+  console.log(passwordCharacter)
+  return passwordCharacter.join('')
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+//Write password to the #password input
+function writePassword() {
+  var password = generatePassword()
+  var passwordText = document.getElementById("password")
+  passwordText.value = password;
+}
+
+
+
+//add event listener to generate button
+generateBtn.addEventListener("click", writePassword) 
+
+
+
+
+
+
+
+
+
+
